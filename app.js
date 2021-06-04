ff8ae9cc675b839e204bf78b37a23d37b5f7dfdc
zeynep-dmrl <56252190+zeynep-dmrl@users.noreply.github.com>
2021-06-04 11:44:53 +0300

    saonuc kutuları düzenlendi

--- app.js
+++ app.js
@@ -5,18 +5,17 @@ function sayfa(){
   for(var i = 0;i<uzunluk;i++){
     Number(veri[i]);
   }
-  //document.getElementById("dataset").innerHTML = veri;
-  document.getElementById("modsonuc").innerHTML ='Sonuc:' + modHesapla(veri);
-  document.getElementById("medyansonuc").innerHTML ='Sonuc:' + medyanHesapla(veri);
-  document.getElementById("sapmasonuc").innerHTML ='Sonuç: ' + standartSapmaHesapla(veri) + ', yaklaşık:' + Math.round(standartSapmaHesapla(veri));
-  document.getElementById("varyanssonuc").innerHTML ='Sonuç: ' + varyansHesapla(veri) + ', yaklaşık: ' + Math.round(varyansHesapla(veri));
-  document.getElementById("aortsonuc").innerHTML ='Sonuç: ' + aritmetikOrtHesapla(veri) + ', yaklaşık: ' + Math.round(aritmetikOrtHesapla(veri));
-  document.getElementById("hortsonuc").innerHTML ='Sonuç: ' + harmonikOrtHesapla(veri) + ', yaklaşık: ' + Math.round(harmonikOrtHesapla(veri));
-  document.getElementById("gortsonuc").innerHTML ='Sonuç: ' + geometrikOrtHesapla(veri) + ', yaklaşık: ' + Math.round(geometrikOrtHesapla(veri));
-  document.getElementById("osapmasonuc").innerHTML ='Sonuç: ' + ortalamaMutlakSapmaHesapla(veri) + ', yaklaşık: ' + Math.round(ortalamaMutlakSapmaHesapla(veri));
-  document.getElementById("dkatsayısısonuc").innerHTML ='Sonuç: ' + degiskenKatsayisiHesapla(veri) + ', yaklaşık: ' + Math.round( degiskenKatsayisiHesapla(veri));
+  document.getElementById("modsonuc").innerHTML ='Sonuc: ' + modHesapla(veri);
+  document.getElementById("medyansonuc").innerHTML ='Sonuc: ' + medyanHesapla(veri)+'<br><br>';
+  document.getElementById("sapmasonuc").innerHTML ='Sonuç: ' + standartSapmaHesapla(veri) + '<br>, yaklaşık:' + Math.round(standartSapmaHesapla(veri));
+  document.getElementById("varyanssonuc").innerHTML ='Sonuç: ' + varyansHesapla(veri) + '<br>, yaklaşık: ' + Math.round(varyansHesapla(veri));
+  document.getElementById("aortsonuc").innerHTML ='Sonuç: ' + aritmetikOrtHesapla(veri) + '<br>, yaklaşık: ' + Math.round(aritmetikOrtHesapla(veri));
+  document.getElementById("hortsonuc").innerHTML ='Sonuç: ' + harmonikOrtHesapla(veri) + '<br>, yaklaşık: ' + Math.round(harmonikOrtHesapla(veri));
+  document.getElementById("gortsonuc").innerHTML ='Sonuç: ' + geometrikOrtHesapla(veri) + '<br>, yaklaşık: ' + Math.round(geometrikOrtHesapla(veri));
+  document.getElementById("osapmasonuc").innerHTML ='Sonuç: ' + ortalamaMutlakSapmaHesapla(veri) + '<br>, yaklaşık: ' + Math.round(ortalamaMutlakSapmaHesapla(veri));
+  document.getElementById("dkatsayısısonuc").innerHTML ='Sonuç: ' + degiskenKatsayisiHesapla(veri) + '<br>, yaklaşık: ' + Math.round( degiskenKatsayisiHesapla(veri));
   histogramOlustur(veri);
 }
-//$("histogram chart").html(histogramOlustur(veri));
+
 
 
